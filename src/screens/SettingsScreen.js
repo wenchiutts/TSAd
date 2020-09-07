c2680226b013fd121c9aac2f64bf450124a7b063
chuck_lien <chuck_lien@htc.com>
2020-09-07 18:12:24 +0800

    implement setting ui item

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,18 +1,66 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import styled from 'styled-components/native';
 import { path } from 'ramda';
+import * as MailComposer from 'expo-mail-composer';
+import dedent from 'dedent';
+import Constants from 'expo-constants';
+
+import IconListItem from 'components/IconListItem';
 
 const StyledView = styled(View)`
   flex: 1;
   justify-content: center;
   align-items: center;
   background-color: ${path(['theme', 'screenBackground'])};
+  padding-horizontal: 20;
+`;
+
+const IconListWithMargin = styled(IconListItem)`
+  margin-top: 12;
 `;
 
 const SettingsScreen = () => (
   <StyledView>
-    <Text>Settingssss!</Text>
+    <IconListItem
+      iconSource={require('assets/icons/settings_contactus.png')}
+      description="Contact Us"
+      onPress={() => {
+        // TODO: CHUCK change the recipients and subject
+        const options = {
+          recipients: ['tikfansapp@gmail.com'],
+          subject: 'TikFans issue',
+          body:
+            '\n\n\n\n\n\n\n' +
+            dedent`
+                        ------ Don't delete infos below ------
+                        user: $user?.uid
+                        platform: $user?.platform
+                        country: $user?.countryCode
+                        uniqueId: $user?.tiktok?.uniqueId
+                        version: ${
+                          process.env?.APP_MANIFEST?.version || Constants.manifest.version
+                        }`,
+        };
+        MailComposer.composeAsync(options);
+      }}
+    />
+    <IconListWithMargin
+      iconSource={require('assets/icons/settings_restore.png')}
+      description="Restore Purchase"
+    />
+    <IconListWithMargin
+      iconSource={require('assets/icons/followstatus_best.png')}
+      description="Rate Us"
+    />
+    <IconListWithMargin
+      iconSource={require('assets/icons/settings_termofuse.png')}
+      description="Term os Use"
+    />
+    <IconListWithMargin
+      iconSource={require('assets/icons/settings_logour.png')}
+      description="Log Out"
+    />
   </StyledView>
 );
 
