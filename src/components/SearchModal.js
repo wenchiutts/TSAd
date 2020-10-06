8f2a8b0fa76502b0517e91360c5f370b769dfe0b
wenctts <wenchiu@toushihtech.com>
2020-10-06 17:48:40 +0800

    Use SearchUserInput to render search user list

--- src/components/SearchModal.js
+++ src/components/SearchModal.js
@@ -1,44 +1,23 @@
 // @format
 import * as React from 'react';
-import { path } from 'ramda';
-import { View, TextInput, Dimensions, Image, Text } from 'react-native';
+import { path, map, values } from 'ramda';
+import { View, Text } from 'react-native';
 import styled from 'styled-components/native';
 import { useDispatch } from 'react-redux';
 
 import Colors from 'constants/Colors';
 import CancelButton from 'components/header/CancelButton';
-import SearchButton from 'components/SearchButton';
 import EmptyFoundView from 'components/EmptyFoundView';
-import { searchUserAction, fetchInsUserPublicAction } from 'modules/instagram/insAuthActions';
 import UserListItem from 'components/UserListItem';
 import apis from 'apis';
-
-const { width: screenWidth } = Dimensions.get('window');
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import SearchUserInput, { useSearchUserInput } from 'components/SearchUserInput';
 
 const StyledView = styled(View)`
   flex: 1;
   justify-content: flex-start;
   align-items: center;
-  padding-top: 70;
-`;
-
-const StyledInput = styled(View)`
-  height: 48;
-  width: ${screenWidth - 40};
-  border-radius: 8;
-  background-color: ${path(['theme', 'primary', 'lightPurple'])};
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: center;
-  padding-left: 16;
-  margin-vertical: 30;
-`;
-
-const StyledTextInput = styled(TextInput)`
-  margin-left: 8;
-  font-size: 14;
-  color: ${path(['theme', 'noticeText'])};
-  font-weight: 500;
+  padding-horizontal: 20;
 `;
 
 const Description = styled(Text)`
@@ -46,21 +25,14 @@ const Description = styled(Text)`
   font-size: 14;
 `;
 
-const LocalUserListItem = ({ item, isExistStory, storyCount, isPrivate }) => (
-  <UserListItem
-    style={{ paddingLeft: 20 }}
-    username={item?.user?.username}
-    profilePicture={item?.user?.profile_pic_url}
-    buttonHide
-    isExistStory={isExistStory}
-    descriptionElement={
-      <Description>
-        {isPrivate
-          ? 'The private account'
-          : `${storyCount} ${storyCount <= 1 ? 'story' : 'stories'}`}
-      </Description>
-    }
-  />
+const LocalUserListItem = ({ fullName, isPrivate, onPress, ...props }) => (
+  <TouchableOpacity onPress={onPress}>
+    <UserListItem
+      {...props}
+      buttonHide
+      descriptionElement={<Description>{isPrivate ? 'The private account' : fullName}</Description>}
+    />
+  </TouchableOpacity>
 );
 
 const SearchModal = ({ navigation }) => {
@@ -72,72 +44,60 @@ const SearchModal = ({ navigation }) => {
     headerLeft: () => <CancelButton onPress={() => navigation.goBack()} />,
   });
 
-  const [isEmpty, setIsEmpty] = React.useState(false);
-  const [isSearched, setIsSearched] = React.useState(false);
-  const [inputText, setInputText] = React.useState('');
-  const [targetUser, setTargetUser] = React.useState('');
-  const [isExistStory, setIsExistStory] = React.useState(false);
-  const [storyCount, setStoryCount] = React.useState(0);
-  const [isPrivate, setIsPrivate] = React.useState(false);
+  const {
+    cleanSearchResult,
+    searchResult,
+    setSearchUserInput,
+    isFocus: isSearchInputFocus,
+    ...searchUserInputOpt
+  } = useSearchUserInput(true, true);
+
+  console.log('searchUserInputOpt', searchUserInputOpt, 'searchResult', searchResult);
 
   const inputEle = React.useRef(null);
+  const [resultStories, setResultStories] = React.useState([]);
 
   const dispatch = useDispatch();
 
-  const onChangeText = text => {
-    setInputText(text);
-  };
-
-  const onPress = async () => {
+  const onPress = user => async () => {
+    if (user?.latest_reel_media === 0) {
+      return;
+    }
     inputEle.current.blur();
-    setIsSearched(true);
-    setIsPrivate(false);
-    const result = await dispatch(searchUserAction(inputText));
-    const targetUser = result.filter(item => item?.user?.username === inputText);
+
     const userStory = await apis.instagram.getStoryDetails({
-      reelIds: targetUser[0]?.user?.pk,
+      reelIds: user?.pk,
     });
-    if (targetUser[0]?.user?.is_private) {
-      setIsPrivate(true);
-    }
-    setIsExistStory(userStory.length !== 0);
-    if (userStory.length !== 0) {
-      setStoryCount(userStory[0].items.length);
-    }
-    if (targetUser.length === 0) {
-      setIsEmpty(true);
-    } else {
-      setIsEmpty(false);
-      setTargetUser(targetUser[0]);
-    }
-    setInputText('');
+    setSearchUserInput(prev => ({
+      ...prev,
+      value: '',
+    }));
+    cleanSearchResult();
+    console.log(userStory[0].items);
   };
 
   return (
     <StyledView>
-      <StyledInput>
-        <SearchButton onPress={onPress} />
-        <StyledTextInput
-          ref={inputEle}
-          value={inputText}
-          onChangeText={onChangeText}
-          placeholder="Search by username"
-          placeholderTextColor={Colors.primary.lightBlue}
-        />
-      </StyledInput>
-      {isSearched ? (
-        isEmpty ? (
-          <EmptyFoundView />
-        ) : (
-          <LocalUserListItem
-            item={targetUser}
-            isExistStory={isExistStory}
-            storyCount={storyCount}
-            isPrivate={isPrivate}
-          />
-        )
-      ) : (
+      <SearchUserInput ref={inputEle} {...searchUserInputOpt} />
+      {Object.keys(searchResult).length !== 0 ? (
+        <View style={{ width: '100%', paddingTop: 10 }}>
+          {map(user => (
+            <LocalUserListItem
+              username={user?.username}
+              key={user.pk}
+              profilePicture={user?.profile_pic_url}
+              isPrivate={user?.is_private}
+              isExistStory={user?.latest_reel_media !== 0}
+              fullName={user?.full_name}
+              isFollowing={user?.friendship_status?.following}
+              onPress={onPress(user)}
+            />
+          ))(values(searchResult))}
+        </View>
+      ) : !searchUserInputOpt.value ? (
         <View></View>
+      ) : (
+        <EmptyFoundView />
       )}
     </StyledView>
   );
